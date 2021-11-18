2f788af496a652755d7e98427907e076f1840fa6
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2021-11-18 17:50:06 +0900

    FIX: fix a bug

--- lib/jga_analysis_qc/report/progress.rb
+++ lib/jga_analysis_qc/report/progress.rb
@@ -94,7 +94,7 @@ module JgaAnalysisQC
       def fastqc_links(sample)
         sample.fastqc_reports.map do |e|
           Render.markdown_link_text(e.read_id, e.html_path)
-        end.join('  ') # newline
+        end.join('<br />')
       end
 
       # @param sample     [Sample]
